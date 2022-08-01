9e12c42fd54a86a35e005384cd6196a47b40305a
kelchan <kelvin.chan131@gmail.com>
2022-08-01 16:52:24 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+List<object> objectList = new List<object>() 
+{
+    7, 28, -1, true, "chair"
+};
+
+var sum = 0;
+List<int> intList = new List<int>();
+foreach( object item in objectList )
+{
+    if( item is int )
+    {
+        // intList.Add( item );
+        sum += (int)item;
+    };
+};
+Console.WriteLine( sum );
